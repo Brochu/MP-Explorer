aa98bd4433aee5d3b916b57cd4c8668c80eaa475
Brochu <alexbrochu1@gmail.com>
2024-01-12 20:50:54 -0500

    [SETUP] Tried to complile with MinGW, many errors, not work it, force clang compiler

--- debug.bat
+++ debug.bat
@@ -1,1 +1,1 @@
-cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Debug
+cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang
--- release.bat
+++ release.bat
@@ -1,1 +1,1 @@
-cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang
