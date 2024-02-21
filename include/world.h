2dd149f25962513d63d5d556a001bcfce25b0f44
Brochu <alexbrochu1@gmail.com>
2024-02-20 19:42:56 -0500

    [DEV] Define geometry data struct for world level rooms

--- include/world.h
+++ include/world.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <DirectXMath.h>
+
 #include <filesystem>
 #include <vector>
 
 #define WORLD_NUM 7
+
+struct Geometry {
+    std::vector<DirectX::XMFLOAT3> pos;
+    std::vector<DirectX::XMFLOAT3> uvs;
+    std::vector<DirectX::XMFLOAT3> norm;
+    std::vector<unsigned int> indices;
+};
 
 struct Room {
     std::filesystem::path path;
