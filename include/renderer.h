026dd2b68ec66d718fe780a03863bf30e77b3964
Brochu <alexbrochu1@gmail.com>
2024-03-01 15:42:48 -0500

    [NOTES] Added notes for next steps to improve renderer

--- include/renderer.h
+++ include/renderer.h
@@ -41,15 +41,20 @@ struct RootSigSample {
 namespace Render {
 
 void Setup(HWND hwnd, int width, int height);
+//TODO: Maybe we don't need this anymore
 void StartFrame(UINT originX, UINT originY, UINT width, UINT height, int rootSigIndex, int psoIndex);
+//TODO Still needed to handle UI and draw + move to next frame
 void EndFrame();
 void Teardown();
 
+//TODO: Should only have one root sig: bindless model
 int CreateRootSignature(std::span<RootSigParam> params, std::span<RootSigSample> samplers);
 int CreatePSO(LPCWSTR shaderFile, LPCWSTR vertEntry, LPCWSTR pixEntry);
+//TODO: Might be able to abstract CB, be able to create SRVs and UAVs
 int CreateBufferedCB(size_t bufferSize);
 Draws UploadDrawData(std::span<UploadData> uploadData);
 
+//TODO: Move functions to impact the current command list with commands
 void BindBufferedCB(int CBIndex, void *data, size_t length);
 void RecordDraws(UINT idxCount, UINT idxStart, INT vertOffset);
 
