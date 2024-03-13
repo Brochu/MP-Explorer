6704b65ff9c0efd40311bb4645f2385eb4634c73
Brochu <alexbrochu1@gmail.com>
2024-03-12 20:18:12 -0400

    [DEV] WIP: Started rework on how we handle mouse motion

--- src/camera.cpp
+++ src/camera.cpp
@@ -15,13 +15,7 @@ XMVECTOR basePos = { -5.f, -5.f, -5.f };
 XMVECTOR baseFwd = { 1.f, 1.f, 1.f };
 XMVECTOR baseUp = { 0.f, 1.f, 0.f };
 
-int32_t lastMouseX = 0;
-int32_t lastMouseY = 0;
-
 Camera initCamera(float width, float height) {
-    // Init mouse data
-    SDL_GetMouseState(&lastMouseX, &lastMouseY);
-
     return { 45.f, (float)width / height, 0.1f, 100000.f, basePos, baseFwd, baseUp };
 }
 
@@ -54,32 +48,21 @@ void updateCamera(SDL_Event *e, CameraInputs &inputs, Camera &cam) {
 
     if (e->type == SDL_MOUSEMOTION) {
         //TODO: Handle updating camera angles with this
-        int x = e->motion.xrel;
-        int y = e->motion.yrel;
-        printf("[CAM] relative mouse motion (%i, %i)\n", x, y);
+        //TODO: Look into setting to invert mouse movements
+        int dx = e->motion.xrel;
+        int dy = e->motion.yrel;
+
+        inputs.theta += (dx * Camera::hsens);
+        if (inputs.theta > 359.f) inputs.theta -= 359;
+        if (inputs.theta < 0.f) inputs.theta += 359;
+        inputs.phi = min(max(inputs.phi - (dy * Camera::vsens), -89.f), 89.f);
+
+        printf("[CAM] relative mouse motion (%i, %i)\n", dx, dy);
+        printf("rotation : theta: %f, phi: %f\n", inputs.theta, inputs.phi);
     }
 }
 
 void moveCamera(Camera &cam, CameraInputs &inputs, float delta, float elapsed) {
-    int currMouseX = 0;
-    int currMouseY = 0;
-    //TODO: Look into capturing mouse pointer in order to stop mouse from going out of bounds
-    SDL_GetMouseState(&currMouseX, &currMouseY);
-    int dx = currMouseX - lastMouseX;
-    int dy = currMouseY - lastMouseY;
-
-    //TODO: Look into setting to invert mouse movements
-    inputs.theta += (dx * Camera::hsens);
-    if (inputs.theta > 359.f) inputs.theta -= 359;
-    if (inputs.theta < 0.f) inputs.theta += 359;
-    inputs.phi = min(max(inputs.phi - (dy * Camera::vsens), -89.f), 89.f);
-
-    //printf("Current mouse position : (%i, %i) [(%i, %i)]\n    theta: %f, phi: %f\n",
-    //       currMouseX, currMouseY, dx, dy, inputs.theta, inputs.phi);
-
-    lastMouseX = currMouseX;
-    lastMouseY = currMouseY;
-
     //TODO: Rotate camera forward vector
     // Rotate the base forward vector with these angles
     // Use rotated forward vector for cam movement
