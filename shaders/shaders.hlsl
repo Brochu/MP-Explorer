c477f58ce9e96f320dc329f6b3704f5fcf00c673
Brochu <alexbrochu1@gmail.com>
2024-03-27 23:21:10 -0400

    [DEV] Update shader code, start swap to bindless Vertex Buffer

--- shaders/shaders.hlsl
+++ shaders/shaders.hlsl
@@ -1,3 +1,4 @@
+#define VBUFFERIDX 0
 ByteAddressBuffer buffers[] : register (t0);
 
 struct Vertex {
@@ -5,23 +6,22 @@ struct Vertex {
     float2 uv;
     float3 normal;
 };
-uint VertexBufferIdx = 0;
 
 cbuffer PerFrame : register(b0) {
     float4x4 mvp;
 };
 
-PSInput VSMain(float3 pos : POSITION, float2 tex : TEXCOORD0) {
-    PSInput o;
-    o.pos = mul(float4(pos, 1.0), mvp);
-    o.color = float4(tex.x, tex.y, 0.0, 1.0);
-    return o;
-}
-
 struct PSInput {
     float4 pos : SV_POSITION;
     float4 color : COLOR;
 };
+
+PSInput VSMain(uint vertId : SV_VertexID) {
+    PSInput o;
+    o.pos = float4(1.0, 1.0, 1.0, 1.0); //mul(float4(pos, 1.0), mvp);
+    o.color = float4(1.0, 1.0, 1.0, 1.0); //float4(tex.x, tex.y, 0.0, 1.0);
+    return o;
+}
 
 float4 PSMain(PSInput input) : SV_TARGET {
     return input.color;
