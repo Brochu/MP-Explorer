bb0c2eb3fb77c87910936e4c7d4bad4c996095b8
Brochu <alexbrochu1@gmail.com>
2024-04-04 23:36:07 -0400

    [DEV] WIP: reworked the command allocator to use c style c++ fully

--- include/cmdallocpool.h
+++ include/cmdallocpool.h
@@ -2,23 +2,26 @@
 #include <d3d12.h>
 #include <mutex>
 #include <queue>
+#include <vector>
 
-namespace Graphics {
+namespace CmdManager {
 #define MAX_ALLOCS 10
 
 struct CmdAllocPool {
-    ID3D12CommandAllocator *RequestAlloc();
-    void DiscardAlloc();
-
-    void Clear();
-
     D3D12_COMMAND_LIST_TYPE type;
-    ID3D12CommandAllocator *allocs[MAX_ALLOCS];
+    ID3D12Device *device;
 
+    std::vector<ID3D12CommandAllocator*> allocPool;
     std::queue<size_t> readyList;
     std::mutex allocMutex;
 };
 
-void InitPool(ID3D12Device *device, D3D12_COMMAND_LIST_TYPE type, CmdAllocPool &pool);
+void CreateAllocPool(CmdAllocPool &pool, ID3D12Device *pDevice);
+void ClearAllocPool(CmdAllocPool &pool);
+
+ID3D12CommandAllocator *RequestAllocator(CmdAllocPool &pool, uint64_t completedValue);
+void DiscardAllocator(CmdAllocPool &pool, uint64_t value, ID3D12CommandAllocator *alloc);
+
+inline size_t AllocPoolSize(CmdAllocPool &pool) { return pool.allocPool.size(); }
 
 }
