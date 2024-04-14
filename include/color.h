410d08ca3a01e2089911a8a9181f1f9a7d00a675
Brochu <alexbrochu1@gmail.com>
2024-04-14 14:36:18 -0400

    [DEV] Added some access functions to color wrapper

--- include/color.h
+++ include/color.h
@@ -3,6 +3,25 @@
 
 struct Color { 
     DirectX::XMVECTORF32 vals;
+
+    float R() const { return DirectX::XMVectorGetX(vals); }
+    float G() const { return DirectX::XMVectorGetY(vals); }
+    float B() const { return DirectX::XMVectorGetZ(vals); }
+    float A() const { return DirectX::XMVectorGetW(vals); }
+
+    void SetR(float r) { vals.f[0] = r; }
+    void SetG(float g) { vals.f[1] = g; }
+    void SetB(float b) { vals.f[2] = b; }
+    void SetA(float a) { vals.f[3] = a; }
+    void SetRGB(float r, float g, float b) {
+        vals.v = DirectX::XMVectorSelect(vals, DirectX::XMVectorSet(r, g, b, 0.f), DirectX::g_XMMask3);
+    };
+
+    float *GetPtr() { return vals.f; }
+    float &operator[](int idx) { return GetPtr()[idx]; }
+
+    bool operator==(const Color &other) { return DirectX::XMVector4Equal(vals, other.vals); }
+    bool operator!=(const Color &other) { return DirectX::XMVector4NotEqual(vals, other.vals); }
 };
 
 Color MakeColor();
