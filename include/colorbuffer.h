50f544a93ae9c6e9b3f1b136221933d906e7af67
Brochu <alexbrochu1@gmail.com>
2024-04-18 19:18:40 -0400

    [DEV] Added different handles for descriptor types in ColorBuffer

--- include/colorbuffer.h
+++ include/colorbuffer.h
@@ -8,6 +8,12 @@ struct ColorBuffer {
     PixelBuffer pix;
 
     Color clearColor;
+    D3D12_CPU_DESCRIPTOR_HANDLE srvHandle;
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle;
+    D3D12_CPU_DESCRIPTOR_HANDLE uavHandle[12];
+    uint32_t numMipMaps;
+    uint32_t fragCount;
+    uint32_t sampleCount;
 };
 
 ColorBuffer CreateColorBuffer(Color clear = MakeColor(0.f, 0.f, 0.f, 0.f));
--- src/colorbuffer.cpp
+++ src/colorbuffer.cpp
@@ -6,6 +6,15 @@ ColorBuffer CreateColorBuffer(Color clear) {
     ColorBuffer cbuf;
     cbuf.pix = CreatePixelBuffer();
     cbuf.clearColor = clear;
+    cbuf.numMipMaps = 0;
+    cbuf.fragCount = 1;
+    cbuf.sampleCount = 1;
+
+    cbuf.srvHandle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+    cbuf.rtvHandle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+    for (size_t i = 0; i < _countof(cbuf.uavHandle); i++) {
+        cbuf.uavHandle[i].ptr = D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN;
+    }
 
     return cbuf;
 }
