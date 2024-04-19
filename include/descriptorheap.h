e5495b41e16435faae5ffa63fd598ee87bca3333
Brochu <alexbrochu1@gmail.com>
2024-04-18 22:31:49 -0400

    [SETUP] Added code files for descriptor management logic

---
+++ include/descriptorheap.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <d3dx12.h>
+
+namespace Graphics {
+
+struct DescriptorAllocator {
+    D3D12_DESCRIPTOR_HEAP_TYPE type;
+};
+
+//--------------------
+struct DescriptorHandle {
+};
+
+//--------------------
+struct DescriptorHeap {
+};
+
+}
