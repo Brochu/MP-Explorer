c10ae0f74dd3a8de0d3ea89930adf9e561d4923a
Brochu <alexbrochu1@gmail.com>
2024-04-19 23:23:29 -0400

    [DEV] Implement RequestNewHeap function

--- src/descriptorheap.cpp
+++ src/descriptorheap.cpp
@@ -1,4 +1,6 @@
 #include "descriptorheap.h"
+#include "graphics.h"
+#include "utility.h"
 
 #include <mutex>
 #include <vector>
@@ -11,8 +13,17 @@ static std::mutex s_allocMutex;
 static std::vector<ComPtr<ID3D12DescriptorHeap>> s_descriptorHeapPool;
 
 static ID3D12DescriptorHeap *RequestNewHeap(D3D12_DESCRIPTOR_HEAP_TYPE type) {
-    //TODO: Impl
-    return nullptr;
+    std::lock_guard<std::mutex> lock(s_allocMutex);
+    D3D12_DESCRIPTOR_HEAP_DESC desc { };
+    desc.Type = type;
+    desc.NumDescriptors = s_numDescriptorPerHeap;
+    desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    desc.NodeMask = 1;
+
+    ComPtr<ID3D12DescriptorHeap> pHeap;
+    ThrowIfFailed(g_device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&pHeap)));
+    s_descriptorHeapPool.emplace_back(pHeap);
+    return pHeap.Get();
 };
 
 DescriptorAllocator MakeDescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type) {
