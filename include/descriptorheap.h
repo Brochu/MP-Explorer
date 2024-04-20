4558e6e80d8d2318ec4f45327e5b2067d7b68c10
Brochu <alexbrochu1@gmail.com>
2024-04-19 22:52:39 -0400

    [DEV] Added all needed members and function for descriptor allocator

--- include/descriptorheap.h
+++ include/descriptorheap.h
@@ -5,7 +5,15 @@ namespace Graphics {
 
 struct DescriptorAllocator {
     D3D12_DESCRIPTOR_HEAP_TYPE type;
+    ID3D12DescriptorHeap *heap;
+    D3D12_CPU_DESCRIPTOR_HANDLE nextHandle;
+    uint32_t descriptorSize;
+    uint32_t numFreeHandles;
 };
+
+DescriptorAllocator MakeDescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type);
+D3D12_CPU_DESCRIPTOR_HANDLE Allocate(DescriptorAllocator &descAllocator);
+void DestroyAll(DescriptorAllocator &descAllocator);
 
 //--------------------
 struct DescriptorHandle {
--- src/descriptorheap.cpp
+++ src/descriptorheap.cpp
@@ -1,1 +1,32 @@
 #include "descriptorheap.h"
+
+#include <mutex>
+#include <vector>
+
+namespace Graphics {
+using namespace Microsoft::WRL;
+
+static const uint32_t s_numDescriptorPerHeap = 256;
+static std::mutex s_allocMutex;
+static std::vector<ComPtr<ID3D12DescriptorHeap>> s_descriptorHeapPool;
+
+static ID3D12DescriptorHeap *RequestNewHeap(D3D12_DESCRIPTOR_HEAP_TYPE type) {
+    //TODO: Impl
+    return nullptr;
+};
+
+DescriptorAllocator MakeDescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type) {
+    //TODO: Impl
+    return { type };
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE Allocate(DescriptorAllocator &descAllocator) {
+    //TODO: Impl
+    return (D3D12_CPU_DESCRIPTOR_HANDLE)0;
+}
+void DestroyAll(DescriptorAllocator &descAllocator) {
+    //TODO: Impl
+}
+
+//--------------------
+}
